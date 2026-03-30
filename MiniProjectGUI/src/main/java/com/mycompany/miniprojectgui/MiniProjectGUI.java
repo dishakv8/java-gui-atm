de6ec6f4386099d6ac10a3afb860c32b53f38b52
Disha K V <kvdisha70@gmail.com>
2026-03-30 19:01:04 +0530

    My first Java GUI app

---
+++ MiniProjectGUI/src/main/java/com/mycompany/miniprojectgui/MiniProjectGUI.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.miniprojectgui;
+
+/**
+ *
+ * @author DISHA K V
+ */
+public class MiniProjectGUI {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
